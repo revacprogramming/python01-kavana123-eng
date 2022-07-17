facd29cdb3f40d935bf38dc63026a2ac6d424b53
Kavana Bankapur <97089741+kavana123-eng@users.noreply.github.com>
2022-07-17 15:30:18 +0000

    completed

--- ActivitySet01/problem10.py
+++ ActivitySet01/problem10.py
@@ -1,3 +1,12 @@
-# Dictionaries
+# list 1
 
-filename = "dataset/mbox-short.txt"
+fname = input("Enter file name: ")
+fh = open(fname)
+lst = list()
+for line in fh:
+    line=line.strip()
+    words=line.split()
+for word in words : 
+    if word not in lst:
+        list.append(word)
+print(sorted(lst))

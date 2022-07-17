cf5ee136815fd4bb77e39db51802518e54572414
Kavana Bankapur <97089741+kavana123-eng@users.noreply.github.com>
2022-07-17 15:32:25 +0000

    completed

--- ActivitySet01/problem11.py
+++ ActivitySet01/problem11.py
@@ -1,3 +1,8 @@
-# Tuples
+# list 2
 
-filename = "dataset/mbox-short.txt"
+fname = input("Enter file name: ")
+if len(fname) < 1:
+    fname = "mbox-short.txt"
+fh = open(fname)
+count = 0
+print("There were", count, "lines in the file with From as the first word")

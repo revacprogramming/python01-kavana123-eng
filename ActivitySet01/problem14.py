dbd93080caf101bd8df1d2bb008b6700f9a91e2b
Kavana Bankapur <97089741+kavana123-eng@users.noreply.github.com>
2022-07-17 15:38:53 +0000

    completed

--- ActivitySet01/problem14.py
+++ ActivitySet01/problem14.py
@@ -1,2 +1,13 @@
-# Using Web Services
-# https://www.py4e.com/lessons/servces
+# Regular expression
+
+import re
+hand=open('file12.txt')
+lst=[]
+for line in hand:
+    line = line.rstrip()
+    x = re.findall('([0-9]+)', line)
+    int_x=list(map(int,x))
+    lst.extend(int_x)
+
+total=sum(lst)
+print('Total:',total)
